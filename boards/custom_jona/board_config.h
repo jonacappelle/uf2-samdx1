8c24ec9867242f772d004c16613f9189bdb70468
jonacappelle <jonacappelle@hotmail.com>
2024-02-09 16:03:02 +0100

    Make bootloader using internal RC oscillator for SAMD21G18A board

--- boards/custom_jona/board_config.h
+++ boards/custom_jona/board_config.h
@@ -1,6 +1,21 @@
 #ifndef BOARD_CONFIG_H
 #define BOARD_CONFIG_H
 
+// #define CRYSTALLESS 1
+
+// #define VENDOR_NAME "DRAMCO"
+// #define PRODUCT_NAME "LabTester"
+// #define VOLUME_LABEL "LABTESTER"
+
+// #define BOARD_ID "DramcoLabTester"
+
+// #define USB_VID 0x2341
+// #define USB_PID 0x024D
+
+// #define LED_PIN PIN_PA08
+
+
+
 #define CRYSTALLESS 1
 
 #define VENDOR_NAME "Adafruit Industries"
@@ -12,6 +27,13 @@
 #define USB_VID 0x239A
 #define USB_PID 0x0015
 
-#define LED_PIN PIN_PA17
+#define LED_PIN PIN_PA08
 
 #endif
+
+
+
+
+
+
+
